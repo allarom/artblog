72eb7eb08dc6fddf3be155b889aaef4c2cf795b8
Alla Roman <alla.roman.mail@gmail.com>
2018-02-27 19:00:38 +0100

    Auth done

--- src/app/pages/signup-page/signup-page.component.ts
+++ src/app/pages/signup-page/signup-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup-page',
@@ -14,26 +17,27 @@ export class SignupPageComponent implements OnInit {
   // ... model (e,g. username: String)
 
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router ) { }
 
   ngOnInit() {
   }
   submitForm(form) {
     this.error = '';
     this.feedbackEnabled = true;
+    const data = {
+      username: this.username,
+      password: this.password
+
+    }
     if (form.valid) {
       this.processing = true;
-      // this.someService.method(... data ...)
-      //   .then((result) => {
-      //     // ... handle result, reset form, etc...
-      //     // ... navigate with this.router.navigate(['...'])
-      //     // ... maybe turn this to false if your're staying on the page - this.processing = false;
-      //   })
-      //   .catch((err) => {
-      //     this.error = err.error.error; // :-)
-      //     this.processing = false;
-      //     this.feedbackEnabled = false;
-      //   });
+      this.authService.signup(data)
+        .then((result) => { return this.router.navigate(['/'])})
+        .catch((err) => {
+          this.error = err.error.error; 
+          this.processing = false;
+          this.feedbackEnabled = false;
+        });
     }
   }
 
